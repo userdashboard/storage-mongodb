66ac3563487593d0e571ba2198f48b81ecf47f6b
Ben Lowry <benlowry@pop-os.localdomain>
2020-06-07 14:44:48 -0600

    Updated with 'addMany'

--- src/storage-list.js
+++ src/storage-list.js
@@ -1,98 +1,211 @@
+const fs = require('fs')
 const MongoDB = require('mongodb')
+const path = require('path')
+const util = require('util')
 
 module.exports = {
-  setup: async (storage, moduleName) => {
+  setup: util.promisify((storage, moduleName, callback) => {
+    if (!callback && moduleName) {
+      callback = moduleName
+      moduleName = null
+    }
     const mongodbURL = process.env[`${moduleName}_MONGODB_URL`] || process.env.MONGODB_URL
+    const dashboardPath1 = path.join(global.applicationPath, 'node_modules/@userdashboard/dashboard/src/log.js')
+    let Log
+    if (fs.existsSync(dashboardPath1)) {
+      Log = require(dashboardPath1)('postgresql-list')
+    } else {
+      const dashboardPath2 = path.join(global.applicationPath, 'src/log.js')
+      Log = require(dashboardPath2)('postgresql-list')
+    }
     const indexedCollections = {}
     let db
-    MongoDB.MongoClient.connect(mongodbURL, (error, mongoClient) => {
+    return MongoDB.MongoClient.connect(mongodbURL, (error, mongoClient) => {
       if (error) {
-        throw error
+        Log.error('error connecting', error)
+        return callback(new Error('unknown-error'))
       }
       const client = module.exports.client = mongoClient
       db = client.db(process.env.MONGODB_DATABASE || 'dashboard')
+      const container = {
+        add: util.promisify((path, itemid, callback) => {
+          return getCollection((error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.insertOne({ path, itemid, created: new Date().getTime() }, { writeConcern: 1 }, (error) => {
+              if (error) {
+                Log.error('error adding', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback()
+            })
+          })
+        }),
+        addMany: util.promisify((items, callback) => {
+          return getCollection((error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            const objects = []
+            const created = new Date().getTime()
+            for (const key in items) {
+              objects.push({ path: key, itemid: items[key], created })
+            }
+            return collection.insertMany(objects, { writeConcern: 1 }, (error) => {
+              if (error) {
+                Log.error('error adding', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback()
+            })
+          })
+        }),
+        count: util.promisify((path, callback) => {
+          return getCollection((error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.countDocuments({ path }, (error, count) => {
+              if (error) {
+                Log.error('error adding', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback(null, count)
+            })
+          })
+        }),
+        exists: util.promisify((path, itemid, callback) => {
+          return getCollection((error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.find({ path, itemid }, { _id: 1 }).toArray((error, result) => {
+              if (error) {
+                Log.error('error checking exists', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback(null, result.length === 1)
+            })
+          })
+        }),
+        list: util.promisify((path, offset, pageSize, callback) => {
+          if (!callback) {
+            if (pageSize) {
+              callback = pageSize
+              pageSize = null
+            } else if (offset) {
+              callback = offset
+              offset = null
+            }
+          }
+          offset = offset || 0
+          if (pageSize === null || pageSize === undefined) {
+            pageSize = global.pageSize
+          }
+          if (offset < 0) {
+            return callback(new Error('invalid-offset'))
+          }
+          return getCollection((error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.find({ path }).sort({ created: -1 }).skip(offset).limit(pageSize).toArray((error, results) => {
+              if (error) {
+                Log.error('error listing', error)
+                return callback(new Error('unknown-error'))
+              }
+              if (!results || !results.length) {
+                return callback()
+              }
+              const items = []
+              for (const result of results) {
+                items.push(result.itemid)
+              }
+              return callback(null, items)
+            })
+          })
+        }),
+        listAll: util.promisify((path, callback) => {
+          return getCollection((error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.find({ path }).sort({ created: -1 }).toArray((error, results) => {
+              if (error) {
+                Log.error('error listing all', error)
+                return callback(new Error('unknown-error'))
+              }
+              if (!results || !results.length) {
+                return callback()
+              }
+              const items = []
+              for (const result of results) {
+                items.push(result.itemid)
+              }
+              return callback(null, items)
+            })
+          })
+        }),
+        remove: util.promisify((path, itemid, callback) => {
+          return getCollection((error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.deleteOne({ path, itemid }, (error) => {
+              if (error) {
+                Log.error('error deleting', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback()
+            })
+          })
+        })
+      }
+      if (process.env.NODE_ENV === 'testing') {
+        container.flush = util.promisify((callback) => {
+          for (const key in indexedCollections) {
+            delete (indexedCollections[key])
+          }
+          return callback()
+        })
+      }
+      return callback(null, container)
     })
-    async function getCollection (file) {
-      let name = 'lists'
-      if (file.indexOf('_') === -1) {
-        name += '-' + file.split('/').join('-')
+
+    function getCollection (callback) {
+      if (indexedCollections.lists) {
+        return callback(null, indexedCollections.lists)
       }
-      if (indexedCollections[name]) {
-        return indexedCollections[name]
-      }
-      const collection = indexedCollections[name] = await db.createCollection(name)
-      const indexes = await collection.indexes()
-      if (indexes && indexes.length) {
-        return collection
-      }
-      await collection.createIndex({ path: 1 }, { collation: { backwards: true, locale: 'en' } })
-      await collection.createIndex({ created: -1 }, { collation: { backwards: true, locale: 'en' } })
-      return collection
+      return db.createCollection('lists', (error, collection) => {
+        if (error) {
+          Log.error('error creating collection', error)
+          return callback(new Error('unknown-error'))
+        }
+        indexedCollections.lists = collection
+        return collection.indexes((error, indexes) => {
+          if (error) {
+            Log.error('error retrieving indexes', error)
+            return callback(new Error('unknown-error'))
+          }
+          if (indexes && indexes.length) {
+            return callback(null, collection)
+          }
+          return collection.createIndex({ path: 1 }, { collation: { backwards: true, locale: 'en' } }, (error) => {
+            if (error) {
+              Log.error('error creating index1', error)
+              return callback(new Error('unknown-error'))
+            }
+            return collection.createIndex({ created: -1 }, { collation: { backwards: true, locale: 'en' } }, (error) => {
+              if (error) {
+                Log.error('error retrieving index2', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback(null, collection)
+            })
+          })
+        })
+      })
     }
-    const container = {
-      add: async (path, itemid) => {
-        const collection = await getCollection(path)
-        const result = await collection.find({ path, itemid }, { _id: 1 }).limit(1).toArray()
-        const existing = result.length === 1
-        if (existing) {
-          return
-        }
-        return collection.insertOne({ path, itemid, created: new Date().getTime() }, { writeConcern: 1 })
-      },
-      count: async (path) => {
-        const collection = await getCollection(path)
-        return collection.countDocuments({ path })
-      },
-      exists: async (path, itemid) => {
-        const collection = await getCollection(path)
-        const result = await collection.find({ path, itemid }, { _id: 1 }).limit(1).toArray()
-        return result.length === 1
-      },
-      list: async (path, offset, pageSize) => {
-        offset = offset || 0
-        if (pageSize === null || pageSize === undefined) {
-          pageSize = global.pageSize
-        }
-        if (offset < 0) {
-          throw new Error('invalid-offset')
-        }
-        const collection = await getCollection(path)
-        const cursor = await collection.find({ path }).limit(pageSize).skip(offset)
-        const items = await cursor.sort({ created: -1 }).toArray()
-        if (!items || !items.length) {
-          return null
-        }
-        const results = []
-        for (const item of items) {
-          results.push(item.itemid)
-        }
-        return results
-      },
-      listAll: async (path) => {
-        const collection = await getCollection(path)
-        const cursor = await collection.find({ path }).sort({ created: -1 })
-        const items = await cursor.toArray()
-        if (!items || !items.length) {
-          return null
-        }
-        const results = []
-        for (const item of items) {
-          results.push(item.itemid)
-        }
-        return results
-      },
-      remove: async (path, itemid) => {
-        const collection = await getCollection(path)
-        return collection.deleteOne({ path, itemid })
-      }
-    }
-    if (process.env.NODE_ENV === 'testing') {
-      container.flush = async () => {
-        for (const key in indexedCollections) {
-          delete (indexedCollections[key])
-        }
-      }
-    }
-    return container
-  }
+  })
 }
--- src/storage.js
+++ src/storage.js
@@ -1,137 +1,284 @@
+const fs = require('fs')
 const MongoDB = require('mongodb')
+const path = require('path')
+const util = require('util')
 
 module.exports = {
-  setup: async (moduleName) => {
+  setup: util.promisify((moduleName, callback) => {
+    if (!callback) {
+      callback = moduleName
+      moduleName = null
+    }
     const mongodbURL = process.env[`${moduleName}_MONGODB_URL`] || process.env.MONGODB_URL
-    let objectsCollection
+    const dashboardPath1 = path.join(global.applicationPath, 'node_modules/@userdashboard/dashboard/src/log.js')
+    let Log
+    if (fs.existsSync(dashboardPath1)) {
+      Log = require(dashboardPath1)('postgresql-list')
+    } else {
+      const dashboardPath2 = path.join(global.applicationPath, 'src/log.js')
+      Log = require(dashboardPath2)('postgresql-list')
+    }
+    const indexedCollections = {}
     let db
-    MongoDB.MongoClient.connect(mongodbURL, (error, mongoClient) => {
+    return MongoDB.MongoClient.connect(mongodbURL, (error, mongoClient) => {
       if (error) {
-        throw error
+        return callback(error)
       }
       const client = module.exports.client = mongoClient
       const database = process.env[`${moduleName}_MONGODB_DATABASE`] || process.env.MONGODB_DATABASE || 'dashboard'
       db = client.db(database)
-    })
-    async function getCollection () {
-      if (objectsCollection) {
-        return objectsCollection
-      }
-      objectsCollection = await db.createCollection('objects')
-      const indexes = await objectsCollection.indexes()
-      if (indexes && indexes.length) {
-        return objectsCollection
-      }
-      await objectsCollection.createIndex({ file: 1 }, { unique: true })
-      return objectsCollection
-    }
-    const container = {
-      exists: async (file) => {
-        if (!file) {
-          throw new Error('invalid-file')
-        }
-        const collection = await getCollection()
-        const result = await collection.find({ file }, { _id: 1 }).limit(1).toArray()
-        return result.length === 1
-      },
-      read: async (file) => {
-        if (!file) {
-          throw new Error('invalid-file')
-        }
-        const collection = await getCollection()
-        const result = await collection.findOne({ file }, { contents: 1 })
-        return result.contents
-      },
-      readMany: async (prefix, files) => {
-        if (!files || !files.length) {
-          throw new Error('invalid-files')
-        }
-        const appended = []
-        for (const i in files) {
-          appended[i] = `${prefix}/${files[i]}`
-        }
-        const collection = await getCollection()
-        const data = {}
-        const array = await collection.find({ file: { $in: appended } }).toArray()
-        for (const i in files) {
-          const itemid = files[i]
-          const key = appended[i]
-          for (const object of array) {
-            for (const field in object) {
-              if (field === key || object[field] === key) {
-                data[itemid] = object.contents
-                break
-              }
-              if (object[field] === itemid) {
-                data[itemid] = object.contents
-                break
-              }
-            }
+      const container = {
+        exists: util.promisify((file, callback) => {
+          if (!file) {
+            return callback(new Error('invalid-file'))
           }
-        }
-        return data
-      },
-      readBinary: async (file) => {
-        if (!file) {
-          throw new Error('invalid-file')
-        }
-        const collection = await getCollection()
-        const result = await collection.findOne({ file }, { buffer: 1 })
-        return result.buffer
-      },
-      write: async (file, contents) => {
-        if (!file) {
-          throw new Error('invalid-file')
-        }
-        if (!contents && contents !== '') {
-          throw new Error('invalid-contents')
-        }
-        const collection = await getCollection()
-        const result = await collection.find({ file }, { _id: 1 }).limit(1).toArray()
-        const existing = result.length === 1
-        if (existing) {
-          await collection.updateOne({ file }, { $set: { contents } }, { writeConcern: 1 })
-          return
-        }
-        return collection.insertOne({ file, contents, created: new Date() }, { writeConcern: 1 })
-      },
-      writeBinary: async (file, buffer) => {
-        if (!file) {
-          throw new Error('invalid-file')
-        }
-        if (!buffer || !buffer.length) {
-          throw new Error('invalid-buffer')
-        }
-        const collection = await getCollection()
-        return collection.insertOne({ file, buffer, created: new Date().getTime() }, { writeConcern: 1 })
-      },
-      delete: async (file) => {
-        if (!file) {
-          throw new Error('invalid-file')
-        }
-        const collection = await getCollection()
-        return collection.deleteOne({ file })
+          return getCollection('objects', (error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.find({ file }, { _id: 1, limit: 1 }, (error, cursor) => {
+              if (error) {
+                Log.error('error checking exists', error)
+                return callback(new Error('unknown-error'))
+              }
+              return cursor.toArray((error, result) => {
+                if (error) {
+                  Log.error('error checking exists', error)
+                  return callback(new Error('unknown-error'))
+                }
+                return callback(null, result.length === 1)
+              })
+            })
+          })
+        }),
+        read: util.promisify((file, callback) => {
+          if (!file) {
+            return callback(new Error('invalid-file'))
+          }
+          return getCollection('objects', (error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.findOne({ file }, { contents: 1 }, (error, result) => {
+              if (error) {
+                Log.error('error reading', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback(null, result.contents)
+            })
+          })
+        }),
+        readMany: util.promisify((prefix, files, callback) => {
+          if (!files || !files.length) {
+            return callback(new Error('invalid-files'))
+          }
+          const appended = []
+          for (const i in files) {
+            appended[i] = `${prefix}/${files[i]}`
+          }
+          return getCollection('objects', (error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            const data = {}
+            return collection.find({ file: { $in: appended } }, (error, cursor) => {
+              if (error) {
+                Log.error('error reading many', error)
+                return callback(new Error('unknown-error'))
+              }
+              return cursor.toArray((error, result) => {
+                if (error) {
+                  Log.error('error reading many', error)
+                  return callback(new Error('unknown-error'))
+                }
+                for (const i in files) {
+                  const itemid = files[i]
+                  const key = appended[i]
+                  for (const object of result) {
+                    for (const field in object) {
+                      if (field === key || object[field] === key) {
+                        data[itemid] = object.contents
+                        break
+                      }
+                      if (object[field] === itemid) {
+                        data[itemid] = object.contents
+                        break
+                      }
+                    }
+                  }
+                }
+                return callback(null, data)
+              })
+            })
+          })
+        }),
+        readBinary: util.promisify((file, callback) => {
+          if (!file) {
+            return callback(new Error('invalid-file'))
+          }
+          return getCollection('objects', (error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.findOne({ file }, { buffer: 1 }, (error, result) => {
+              if (error) {
+                Log.error('error reading binary', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback(null, result.buffer)
+            })
+          })
+        }),
+        write: util.promisify((file, contents, callback) => {
+          if (!file) {
+            return callback(new Error('invalid-file'))
+          }
+          if (!contents && contents !== '') {
+            return callback(new Error('invalid-contents'))
+          }
+          return getCollection('objects', (error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.find({ file }, { _id: 1, limit: 1 }, (error, cursor) => {
+              if (error) {
+                Log.error('error writing', error)
+                return callback(new Error('unknown-error'))
+              }
+              return cursor.toArray((error, result) => {
+                if (error) {
+                  Log.error('error writing', error)
+                  return callback(new Error('unknown-error'))
+                }
+                const existing = result.length === 1
+                if (existing) {
+                  return collection.updateOne({ file }, { $set: { contents }, writeConcern: 1 }, (error) => {
+                    if (error) {
+                      Log.error('error writing', error)
+                      return callback(new Error('unknown-error'))
+                    }
+                    return callback()
+                  })
+                }
+                return collection.insertOne({ file, contents, created: new Date() }, { writeConcern: 1 }, (error) => {
+                  if (error) {
+                    Log.error('error writing', error)
+                    return callback(new Error('unknown-error'))
+                  }
+                  return callback()
+                })
+              })
+            })
+          })
+        }),
+        writeBinary: util.promisify((file, buffer, callback) => {
+          if (!file) {
+            return callback(new Error('invalid-file'))
+          }
+          if (!buffer || !buffer.length) {
+            return callback(new Error('invalid-buffer'))
+          }
+          return getCollection('objects', (error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.insertOne({ file, buffer, created: new Date().getTime() }, { writeConcern: 1 }, (error) => {
+              if (error) {
+                Log.error('error writing binary', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback()
+            })
+          })
+        }),
+        delete: util.promisify((file, callback) => {
+          if (!file) {
+            return callback(new Error('invalid-file'))
+          }
+          return getCollection('objects', (error, collection) => {
+            if (error) {
+              return callback(error)
+            }
+            return collection.deleteOne({ file }, (error) => {
+              if (error) {
+                Log.error('error deleting', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback()
+            })
+          })
+        })
       }
-    }
-    if (process.env.NODE_ENV === 'testing') {
-      const util = require('util')
-      container.flush = util.promisify((callback) => {
-        async function doFlush () {
+      if (process.env.NODE_ENV === 'testing') {
+        const util = require('util')
+        container.flush = util.promisify((callback) => {
+          function doFlush () {
+            if (!db) {
+              return setTimeout(doFlush, 1)
+            }
+            return db.collections((error, collections) => {
+              if (error) {
+                Log.error('error flushing', error)
+                return callback(new Error('unknown-error'))
+              }
+              function nextItem () {
+                if (!collections.length) {
+                  return callback()
+                }
+                const collection = collections.shift()
+                return collection.deleteMany({}, (error) => {
+                  if (error) {
+                    Log.error('error flushing', error)
+                    return callback(new Error('unknown-error'))
+                  }
+                  return nextItem()
+                })
+              }
+              return nextItem()
+            })
+          }
           if (!db) {
             return setTimeout(doFlush, 1)
           }
-          const collections = await db.collections()
-          for (const collection of collections) {
-            await collection.deleteMany({})
+          return doFlush()
+        })
+      }
+      return callback(null, container)
+    })
+
+    function getCollection (callback) {
+      if (indexedCollections.objects) {
+        return callback(null, indexedCollections.objects)
+      }
+      return db.createCollection('objects', (error, collection) => {
+        if (error) {
+          Log.error('error creating collection', error)
+          return callback(new Error('unknown-error'))
+        }
+        indexedCollections.objects = collection
+        return collection.indexes((error, indexes) => {
+          if (error) {
+            Log.error('error retrieving indexes', error)
+            return callback(new Error('unknown-error'))
           }
-          return callback()
-        }
-        if (!db) {
-          return setTimeout(doFlush, 1)
-        }
-        return doFlush()
+          if (indexes && indexes.length) {
+            return callback(null, collection)
+          }
+          return collection.createIndex({ path: 1 }, { collation: { backwards: true, locale: 'en' } }, (error) => {
+            if (error) {
+              Log.error('error creating index1', error)
+              return callback(new Error('unknown-error'))
+            }
+            return collection.createIndex({ created: -1 }, { collation: { backwards: true, locale: 'en' } }, (error) => {
+              if (error) {
+                Log.error('error retrieving index2', error)
+                return callback(new Error('unknown-error'))
+              }
+              return callback(null, collection)
+            })
+          })
+        })
       })
     }
-    return container
-  }
+  })
 }
